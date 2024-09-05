f771de1fc403f488e738de3289b05805ccea176b
Ben <gbsayou@gmail.com>
2024-09-05 12:49:57 +0800

    update metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 import {Open_Sans} from 'next/font/google';
+import Head from 'next/head';
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -9,6 +10,9 @@ const openSans = Open_Sans({
 export default function App({Component, pageProps}: AppProps) {
   return (
     <main className={openSans.className}>
+      <Head>
+        <title>Ben Ge</title>
+      </Head>
       <Component {...pageProps} />
     </main>
   );
